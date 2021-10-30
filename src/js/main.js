65fd201ce155c1c41698241a13b702e3d38ee51c
Christian Vallat <cmvallat@live.unc.edu>
2021-10-29 21:11:08 -0400

    Partially working navbar and basic page 3

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
+    console.log("event happened");
     this.classList.toggle("active");
 
     /* Toggle between hiding and showing the active panel */
@@ -18,16 +19,23 @@ for (i = 0; i < acc.length; i++) {
 } 
 
 
+
 // Reference hamburger icon
-var button = document.getElementsByClassName("hamburger");
-var ul = document.getElementsByClassName("pizza");
+var button = document.getElementById("hamburger");
+
+var ul = document.getElementById("pizza");
 
 // Show nav
 function toggle() {
-  ul.classList.toggle("active");
+  
 }
 
 // Wire up the event
-button.addEventListener("click", toggle);
+button.addEventListener("click", function(){
+  /*ul.classList.toggle("active");*/
+  console.log("toggled");
+
+});
+
 
 
